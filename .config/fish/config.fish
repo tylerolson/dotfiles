05ee17815b661aa9a2a50c9a076e8c976dc2d4da
Tyler Olson <tolson519@gmail.com>
2025-08-19 23:23:05 -0700

    move paths to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+fish_add_path --path $HOME/go/bin
+
 starship init fish | source
